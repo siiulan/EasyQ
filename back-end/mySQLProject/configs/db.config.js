a5a2f47c863b23fbb25056656b1b9aadde6f799b
siiulan <siiulan79@gmail.com>
2022-02-08 11:42:38 -0500

    AWS DB password config add
    adding the password for EC2 ubuntu's database

--- back-end/mySQLProject/configs/db.config.js
+++ back-end/mySQLProject/configs/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: 'localhost',
     user: 'root',
-    password: 'Qwe.1234',
+    password: "ECE651Winter2022#16" ,
+    // password: 'Qwe.1234',
     database: 'test'
 };
