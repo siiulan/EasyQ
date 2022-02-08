ba60915ca576a8975535155836fa9acba4bc016d
siiulan <siiulan79@gmail.com>
2022-02-08 12:59:16 -0500

    Update db.config.js

--- back-end/mySQLProject/configs/db.config.js
+++ back-end/mySQLProject/configs/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 'localhost',
     user: 'root',
-    // password: "ECE651Winter2022#16" ,
-    password: 'Qwe.1234',
+    password: "ECE651Winter2022#16" ,
+    // password: 'Qwe.1234',
     database: 'test'
 };
