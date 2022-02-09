77cab9f75d4086bed3105a174c20344bd4154785
Yuxin Tang <y229tang@uwaterloo.ca>
2022-02-09 16:16:58 -0500

    add a resetpwd success page

---
+++ Desktop/test/EasyQ/src/views/ResetSuccess.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="alert alert-success">
+        <strong>Success!</strong> The password has been reset.
+    </div>
+</template>
+
+<script>
+ </script>
+
+ <<style scoped>
+ </style>
