45636de3ec4cb3d5ae265e68c79ac1eb7a68c46e
Yuxin Tang <y229tang@uwaterloo.ca>
2022-02-24 02:14:08 -0500

    export NODE_OPTIONS=--openssl-legacy-provider

--- Frontend/EasyQ/src/main.js
+++ Frontend/EasyQ/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './../node_modules/bulma/css/bulma.css';
+import '../node_modules/bulma/css/bulma.css';
 
 // Import the Auth0 configuration
 // import { domain, clientId } from "../auth_config.json";
