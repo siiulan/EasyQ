cac5cd319032c09b24ddbd19c2f7ad71fc7b46a6
Shuoni Xu <s358xu@uwaterloo.ca>
2022-03-13 04:57:51 -0400

    Sprint2 Student

---
+++ Frontend/EasyQ/src/components/StudentClassCard.vue
@@ -0,0 +1,133 @@
+<template>
+  <!-- <div class="student-class-card">
+    <div class="card1" >
+      <div class="card-content">
+        <h2 class="is-size-4 has-text-weight-bold">ECE651</h2>
+        <h2 class="is-size-4 has-text-weight-bold">Foundations Software Engineering</h2>
+        <h2 class="is-size-6">Krzysztof Czarnecki</h2>
+        <button class="button" style="margin-right : 40px">
+            <router-link to = "/StudentOfficeHour">Join</router-link>
+        </button>
+      </div>
+    </div>
+  </div>
+</div> -->
+  <div id="main" class="d-flex flex-column ms-5">
+    <div class="ms-5 mb-3 h-25 h3">Course List</div>
+    <li v-for="item in items" :key="item.CLASS_NAME">
+        <!-- <router-link :to="'/TAQueue/' + item.CLASS_NUMBER" class="ms-5 mb-3 h-25 w-75 border shadow-sm d-flex flex-column"> -->
+          <div class="ms-2 mt-2 fs-5">{{ item.CLASS_NAME }}</div>
+          <div class="ms-2 mb-3 d-flex flex-row fs-6 w-100">
+              <div class="col-xl-3">Instructor: {{item.INSTRUCTOR_NAME}}</div>
+              <!-- <div class="col-xl-4">Schedule: {{item.schedule}}</div> -->               
+          </div>
+          <button class="button" style="margin-right : 40px">
+            <router-link to = "'/StudentOfficeHour/' + item.CLASS_ID">Join</router-link>
+          </button>
+          <button class="button" style="margin-right : 80px">
+            <router-link to = "'/EventSingle/' + item.CLASS_ID">Info</router-link>
+          </button>
+        <!-- </router-link> -->
+    </li>
+    <div class="card text-center" style="margin-top:20px">
+        <div class="card-body" >
+          <h5 class="card-title">Need to add class?</h5>
+          <a href="#" class="btn btn-primary" style="font-weight:bold; background-color: #a3b6f1; border: none;">
+            <router-link to = "'/StudentAddClassInfo/' + item.CLASS_ID">Add</router-link>
+          </a>
+      </div>
+    </div>
+  </div>
+  
+</template>
+  
+<script>
+import axios from 'axios'
+  export default {
+    mounted: function(){
+      this.getInfo();
+    },
+    data() {
+      return{
+        userId: getCookie("id"),
+        // classNumber: '',
+        // className: '',
+        // instructorName: '',
+        // classId: ''
+        items: []
+      };
+    },
+    methods:{
+      async getInfo() {
+        var data = {
+          userId: this.userId,
+        };
+        const response = await axios.post('http://54.163.38.93/api/user/student/classes', data, {headers:{'Content-type': 'application/json',}});
+        // this.classNumber = response.CLASS_NUMBER;
+        // this.className = response.CLASS_NAME;
+        // this.instructorName = response.INSTRUCTOR_NAME;
+        // this.classId = response.CLASS_ID;
+        this.items = response.data.course_list;
+      }
+    }
+  };
+
+  function getCookie(cname){
+    let name = cname + "=";
+    let decodeCookie = decodeURIComponent(document.cookie);
+    let ca = decodeCookie.split(';');
+    for(let i = 0; i < ca.length; i++){
+      let c = ca[i];
+      while(c.charAt(0) == ' '){
+        c = c.substring(1);
+      }
+      if(c.indexOf(name) == 0){
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+</script>
+<style lang="scss" scoped>
+  .card1 {
+    height: 150px;
+    width: 1300px;
+    background-color: rgba(248, 227, 196, 0.35);
+    background-size: cover;
+    display: block;
+  }
+  .card-content {
+        padding-top: 50px;
+        // position: absolute;
+        color: rgb(0, 0, 0);
+        background-color: rgba(248, 227, 196, 0.35);
+        top: 0;
+        padding: 10px;
+        height: 150px;
+        margin: 2;
+    h2 {
+      text-align: left;
+      margin-left: 30px;
+      margin-top: 15px;
+    }
+    button {
+        background-color: #a3b6f1;
+        border: none;
+        color: white;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 20px;
+        transition-duration: 0.4s;
+        float: right;
+        margin-top: -70px;
+        display: block;
+        text-align: center;
+        font-weight: bold;
+    }
+    button:hover{
+        background-color: #4caf50;
+        color: white;
+    }
+    
+  }
+</style>
--- Frontend/EasyQ/src/views/EventSingle.vue
+++ Frontend/EasyQ/src/views/EventSingle.vue
@@ -3,36 +3,69 @@
     <section class="hero is-primary">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">Event name</h1>
-          <h2 class="subtitle ">Event date</h2>
+          <h1 class="title">Class Number: {{classNumber}}</h1>
+          <h2 class="subtitle ">Class Name: {{className}}</h2>
         </div>
       </div>
     </section>
     <section class="event-content">
       <div class="container">
-        <p class="is-size-4 description">Event description</p>
-        <p class="is-size-4">Location:</p>
-        <p class="is-size-4">Category:</p>
-        <div class="event-images columns is-multiline has-text-centered">
+        <p class="is-size-4 description">Detailed info:</p>
+        <p class="is-size-4">instructor name: {{instructorName}}</p>
+        <p class="is-size-4">TA Name: {{TAName}}</p>
+        <!-- <div class="event-images columns is-multiline has-text-centered">
           <div class="column is-one-third">IMAGE PLACEHOLDER</div>
-        </div>
+        </div> -->
       </div>
     </section>
   </div>
 </template>
 <script>
-  export default {};
+import axios from 'axios';
+
+  export default {
+      data() {
+          return{
+              className: '',
+              classNumber: '',
+              classId: '',
+              TAName: '',
+              instructorName: ''
+          }
+      },
+      
+      async getClassInfo() {
+        var data = {
+          classId : this.$route.params.classId,
+        }
+        const response = await axios.post('http://54.163.38.93/api/user/student//classes/class',{data},{headers: {'Content-type' : 'application/json',}});
+        this.className = response.CLASS_NAME;
+        this.classId = response.CLASS_ID;
+        this.classNumber = response.CLASS_NUMBER;
+        this.TAName = response.TA_NAME;
+        this.instructorName = response.INSTRUCTOR_NAME;
+        this.classId = response.CLASS_ID
+      }
+    // CLASS_ID 
+    // CLASS_NUMBER 
+    // CLASS_NAME 
+    // CLASS_INFO 
+    // CLASS_TERM 
+    // INSTRUCTOR_NAME 
+    // TA_NAME //all the TA’s name, not just one
+
+  };
 </script>
 <style lang="scss" scoped>
   .event-single {
-    margin-top: 30px;
+    margin-top: 20px;
   }
   .hero {
     margin-bottom: 70px;
   }
-  .event-images {
-    margin-top: 50px;
-  }
+  // .event-images {
+  //   margin-top: 50px;
+  // }
   .description {
     margin-bottom: 30px;
   }
