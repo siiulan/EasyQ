03d5965f82628e7a4a07f71c41a746bba1ec075b
hongyao1307 <hongyaosong1307@gmail.com>
2022-03-16 23:58:55 -0400

    test

--- back-end/mySQLProject/controllers/student.controller.js
+++ back-end/mySQLProject/controllers/student.controller.js
@@ -118,7 +118,7 @@ exports.intheOffice_con = (req, res) => {
         })
     }
     let user_id = req.body.userId;
-    let officehour_id = req.body.office_hour_Id;
+    let officehour_id = req.body.officehourId;
     console.log('controller officehour', officehour_id)
     let class_id = req.body.classId;
     Student.intheOffice(user_id, officehour_id, class_id, (err, data) => {
