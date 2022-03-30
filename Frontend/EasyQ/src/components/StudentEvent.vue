1d0ebe5775692f0d2f4f2f50348eb7e282e60f86
sharonuu <sharonxu1030@outlook.com>
2022-03-30 16:44:54 -0400

    modify UI for student Home page

--- Frontend/EasyQ/src/components/StudentEvent.vue
+++ Frontend/EasyQ/src/components/StudentEvent.vue
@@ -45,9 +45,3 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-.events {
-  margin-top: 100px;
-  text-align: center;
-}
-</style>
--- Frontend/EasyQ/src/views/StudentHome.vue
+++ Frontend/EasyQ/src/views/StudentHome.vue
@@ -18,13 +18,4 @@ export default {
     },
 };
 </script>
-<style lang="scss" scoped>
-  // .card1 {
-  //   height: 150px;
-  //   width: 1300px;
-  //   background-color: rgba(248, 227, 196, 0.35);
-  //   background-position: center;
-  //   background-size: cover;
-  //   display: block;
-  // }
-</style>
+
